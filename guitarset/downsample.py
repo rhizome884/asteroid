b31094e7b9ed80719bc06495ff454766084a3635
c22091860 <hulmeed@cardiff.ac.uk>
2024-11-08 12:22:37 +0000

    downsample script, 8000 samples mathces MiniLibriMix sample rate

---
+++ guitarset/downsample.py
@@ -0,0 +1,19 @@
+import os
+import librosa
+import soundfile as sf
+
+from utils import print_wav_info
+
+audiodir = 'dataset_downsample'
+samplerate = 8000
+
+for dirpath, dirnames, filenames in os.walk(audiodir):
+    if dirpath != audiodir:
+        for f in filenames:
+            fpath = os.path.join(dirpath, f)
+            print_wav_info(fpath)
+            y, sr = librosa.load(fpath, sr=samplerate)
+            sf.write(fpath, y, sr)
+            print_wav_info(fpath)
+
+print('Done!')
